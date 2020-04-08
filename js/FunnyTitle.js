71bf40d61c74dc0be9637144a668db3c3df9feb5
yzyyz1387 <1796031384@qq.com>
2020-04-09 02:33:02 +0800

    Site updated: 2020-04-09 02:33:02

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -4,12 +4,12 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         $('[rel="icon"]').attr('href', "/funny.ico");
-        document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+        document.title = '我去!(°A° 页面崩溃啦 ~';
         clearTimeout(titleTime);
     }
     else {
         $('[rel="icon"]').attr('href', "/favicon.ico");
-        document.title = '(ฅ>ω<*ฅ) 骗你的啦~' + OriginTitle;
+        document.title = ' 开玩笑哒哈哈哈~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
         }, 2000);
